b3d90eb40b9bdd1f1ad7589db96fbd3f64d91b4b
clemenrz <clemenrz@sheridanc.on.ca>
2022-04-20 15:33:45 -0400

    Format Suits enum and author comment

--- src/CardsAndDecks/Suits.java
+++ src/CardsAndDecks/Suits.java
@@ -1,7 +1,14 @@
 package CardsAndDecks;
 
+/**
+ * 
+ *
+ * @author Marcelo, Rzez, Uzair, Hannah April 2022
+ */
+
 public enum Suits {
-    
-    HEARTS, DIAMONDS, CLUBS, SPADES
-    
+    HEARTS, 
+    DIAMONDS, 
+    CLUBS, 
+    SPADES
 }
