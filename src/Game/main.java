1c3ecc69f5f4f56b8e0a9086797e48a51a55642e
clemenrz <clemenrz@sheridanc.on.ca>
2022-04-21 22:46:18 -0400

    Finish refactoring Dialogue and GameView classes and comments

--- src/Game/main.java
+++ src/Game/main.java
@@ -1,5 +1,11 @@
 package Game;
 
+/**
+ * main class that runs the entire program.
+ *
+ * @author Rzez, Marcelo, Uzair, Hannah April 2022
+ */
+
 public class main {
 
     public static void main(String[] args) throws InterruptedException {
